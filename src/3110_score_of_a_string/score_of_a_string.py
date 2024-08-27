937534fa1d953ec36b7aa962f83898422b42406c
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2024-08-27 09:33:58 -0300

    :sparkles: feat: Add 3110_score_of_a_string

---
+++ src/3110_score_of_a_string/score_of_a_string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        varSum = 0
+        for i in range(len(s) - 1):
+            varSum += abs(ord(s[i]) - ord(s[i + 1]))
+        return varSum
