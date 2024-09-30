eb7347c84071a6b81eb8d1b5697df92d356869c6
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2024-09-30 07:42:36 -0300

    :sparkles: feat: Add new solution 2710_remove_trailing_zeros_from_a_string

--- src/2710_remove_trailing_zeros_from_a_string/main.py
+++ src/2710_remove_trailing_zeros_from_a_string/main.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type num: str
         :rtype: str
         """
-        ans = ''
         count = 0
         for i in range(len(num) - 1, -1, -1):
             if num[i] == '0':
                 count += 1
             else:
                 break
-        for i in range(len(num) - count):
-            ans += num[i]
-        return ans
+        if count == 0:
+            return num
+        return num[:-count]
