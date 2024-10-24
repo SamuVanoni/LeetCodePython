777c06a2c97715dd89d0b30b88ce441f7f68259c
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2024-10-24 08:47:33 -0300

    :sparkles: feat: Add 0977_squares_of_a_sorted_array

---
+++ src/0977_squares_of_a_sorted_array/main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for num in nums:
+            ans.append(num * num)
+        ans.sort()
+        return ans
