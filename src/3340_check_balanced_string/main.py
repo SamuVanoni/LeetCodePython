e53e9bd37589d4aa5038439bb0d6204638ab5b67
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2025-01-03 08:38:29 -0300

    :sparkles: feat: Add 3340_check_balanced_string

---
+++ src/3340_check_balanced_string/main.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isBalanced(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        count1 = 0
+        count2 = 0
+        for i in range(len(num)):
+            if i % 2 == 0:
+                count1 += int(num[i])
+            else:
+                count2 += int(num[i])
+        return count1 == count2
