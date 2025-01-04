6553a3b503e35ddb8207fde37614325220485d69
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2025-01-04 08:47:05 -0300

    :sparkles: feat: Add 1588_sum_of_all_odd_length_subarrays

---
+++ src/1588_sum_of_all_odd_length_subarrays/main.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def sumOddLengthSubarrays(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        n = len(arr)
+        answer = 0
+        
+        for left in range(n):
+            for right in range(left, n):
+                if (right - left + 1) % 2 == 1:
+                    current_sum = 0
+                    for index in range(left, right + 1):
+                        current_sum += arr[index]
+                    answer += current_sum
+        return answer
