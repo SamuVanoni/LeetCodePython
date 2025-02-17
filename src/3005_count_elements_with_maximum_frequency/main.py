4b916a374b0c7e7b5214347f6271c2accde1ac18
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2025-02-17 09:05:57 -0300

    :sparkles: feat: Add 3005_count_elements_with_maximum_frequency

---
+++ src/3005_count_elements_with_maximum_frequency/main.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxFrequencyElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Find the frequency of each element
+        frequencies = {}
+        for num in nums:
+            if num in frequencies:
+                frequencies[num] += 1
+            else:
+                frequencies[num] = 1
+
+        # Determine the maximum frequency
+        max_frequency = 0
+        for frequency in frequencies.values():
+            max_frequency = max(max_frequency, frequency)
+
+        # Calculate the total frequencies of elements with the maximum frequency
+        frequency_of_max_frequency = 0
+        for frequency in frequencies.values():
+            if frequency == max_frequency:
+                frequency_of_max_frequency += 1
+
+        return frequency_of_max_frequency * max_frequency
